a3b539bd84f88a0c4190edb0c74dec5cdf417c31
David Daskal <DaskalDV@Davids-MBP.home>
2016-03-15 15:43:01 -0400

    first edit

---
+++ assets/javascript/bands.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+
+var bands = ["Pink Floyd","David Bowie","Neil Young","Joy Division","Psychedelic Furs","The Beatles","Billy Joel"];
+
+
+for (var i = 0;i < bands.length;i++) {
+	var a = $('<button>');
+	a.attr("class","buttons");
+	a.text(bands[i]);
+	a.attr("data-name",bands[i]);
+	$("#banddisplay").append(a);
+}
+
+$("#addband").on("click", function() {
+
+	var newBand = $("#musicinput").val().trim();
+	bands.push(newBand);
+	
+		var a = $('<button>');
+		a.text(newBand);
+		a.attr("class","buttons");
+		a.attr("data-name",newBand);
+		$("#banddisplay").append(a);
+		
+	
+return false;
+	});
+
+
+
+function showImage() {
+
+
+$("#bandpic").empty();
+currentBand = $(this).attr("data-name");
+//alert (currentBand);
+
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + currentBand + "&api_key=dc6zaTOxFJmzC&limit=8";
+	$.ajax ({
+			url: queryURL,
+			method: 'GET'
+	})
+		.done(function(response) {
+			for (i = 0;i<response.data.length;i++) {
+			pic = $("<img>");
+			rating = $("<p>");
+			still = response.data[i].images.fixed_height_still.url;
+			animated = response.data[i].images.fixed_height.url;
+			pic.addClass("bandphoto");
+			pic.attr("src",still);
+			pic.attr("data-still",still);
+			pic.attr("data-animated",animated);
+			pic.attr("data-state","still");
+			rating.html("Rating: " + response.data[i].rating);
+			$("#bandpic").append(rating);
+			$("#bandpic").append(pic);
+			if (i == response.data.length - 1) {
+			animationStatus(); } // only works when inside the loop. Not efficient to run each time util all images are populated
+				
+				}  //end of for loop to show and assign data to images
+					});
+
+		
+
+
+return false;
+}
+
+
+function animationStatus() {
+$(".bandphoto").on("click",function () {
+				
+				var currentState = $(this).attr("data-state");
+				if (currentState == "still"){
+                $(this).attr("src", $(this).data("animated"));
+                $(this).attr("data-state", "animated");
+            }else{
+                $(this).attr("src", $(this).data("still"));
+                $(this).attr("data-state", "still");
+            }
+			}); 
+}
+
+	
+
+$(document).on('click', '.buttons', showImage);
+
+
+});
